85daf1942dd63119c8639e8bce1d9b0bd6b356f0
MFarrell242 <farrell.michael.w@gmail.com>
2019-12-18 19:46:06 -0600

    server serves

--- server.js
+++ server.js
@@ -33,13 +33,13 @@ var waiting = [
 //-----------------------------------------------
 //GET
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, ""))
+    res.sendFile(path.join(__dirname, "home.html"))
 });
 app.get("/reserve", function(req, res){
-    res.sendFile(path.join(__dirname, ""))
+    res.sendFile(path.join(__dirname, "reserve.html"))
 });
 app.get("/tables", function(req, res){
-    res.sendFile(path.join(__dirname, ""))
+    res.sendFile(path.join(__dirname, "tables.html"))
 });
 app.get("/api/tables", function(req,res){
     return res.json(tables)
